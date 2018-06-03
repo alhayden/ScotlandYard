0c6f57f1651e5fe73f4fe011506d1dbc3d073b31
Dylan Jones <dylanjones2011@gmail.com>
2018-06-03 15:15:45 -0400

    game

---
+++ engine/game.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+class Game:
+    def __init__(self, mr_x, detectives):
+        pass # TODO: do some initalization
+
+    def next_turn(self):
+        pass # TODO: run the next turn
+
+    def next_round(self):
+        pass # TODO: run the next roundA
+
+    # self.detectives: an array of Player objects, but only the detcvitvs
+    # self.x: mr. x, a player object
