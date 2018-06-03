cf95b60268de9d77171a4b5e49cc5c0615ae2ab7
1F335 <alexhayden25@gmail.com>
2018-06-03 17:59:03 -0400

    Complete init and next_round in game

--- engine/game.py
+++ engine/game.py
@@ -5,11 +5,22 @@ import random
 
 class Game:
     def __init__(self, mr_x, detectives):
+        try:
+            import mrx
+        except:
+            print("Failed to load Mr. X AI.  Make sure it is in this directory and named 'mrx.py'")
+        try:
+            import detectives
+        except:
+            print("Failed to load the detective AI.  Make sure it is in this directory and named 'detectives.py'")
+
         self.boardmap = {}
         self.x_history = []
         self.detectives = []
         self.x = None
         self.load_board()
+        self.round = 0
+        self.reveal_rounds = [3, 8, 13, 18, 24]
         
         
         startTickets = {"taxi" : 10, "bus" : 8, "underground" : "4"}
@@ -32,7 +43,62 @@ class Game:
         pass # TODO: run the next turn
 
     def next_round(self):
-        pass # TODO: run the next roundA
+        self.round += 1
+
+        detectives_public = [copy.deepcopy(p) for p in self.detectives]
+        x_public = copy.deepcopy(self.x)
+
+        mrx.play_move(x_public, detectives_public, copy.deepcopy(self.x_history))
+
+        if self.x != x_public:
+            self.gameEnd(False) ## X loses
+        if self.moveValid(x_public):
+            self.x.pos = x_public.nextMove[0]
+            transport = x_public.nextMove[1]
+            self.x.tickets[transport] = self.x.tickets[transport] - 1
+            if self.x.tickets[transport] < 0:
+                self.gameEnd(False) ## X loses
+
+            self.x_history.append((self.x.pos, transport))
+
+        self.checkGameOver()
+        
+        for i in range(len(self.x_history)):
+            if i in self.reveal_rounds:
+                x_history_public.append(self.x_history[i])
+            else:
+                x_history_public.append((None, self.x_history[i][1]))
+
+        detectives.play_move(detectives_public, copy.deepcopy(x_history_public))
+
+
+        for i in range(len(self.detectives)):
+            detective = self.detectives[i]
+            detective_public = detectives_public[i]
+            if detective != detective_public:
+                self.gameEnd(True) #X wins
+
+            if detective_public.nextMove == None and self.cantMove(detective_public):
+                continue
+
+            if self.moveValid(detective_public):
+                detective.pos = detective_public.nextMove[0]
+                transport = detective_public.nextMove[1]
+                detective.tickets[transport] = detective.tickets[transport] - 1
+                if detective.tickets[transport] < 0:
+                    self.gameEnd(True) ## X wins
+
+        self.checkGameOver()
+
+    def moveValid(self, player):
+        return player.nextMove[0] in self.boardmap[player.pos][player.nextMove[1]]
+
+    def cantMove(self, player):
+        for ticket in player.tickets.keys():
+            if player.tickets[ticket] > 0 and ticket in self.boardmap[player.pos]:
+                return False
+        return True
+
 
     def load_board(self):
         t = "t"
