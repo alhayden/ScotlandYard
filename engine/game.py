580a71f62f2bec97fe201eb0f4538a1b64f1d730
1F335 <alexhayden25@gmail.com>
2018-06-03 16:10:24 -0400

    Add init for game

--- engine/game.py
+++ engine/game.py
@@ -1,15 +1,66 @@
 #!/bin/python3
 
+import random
+
+
 class Game:
     def __init__(self, mr_x, detectives):
-       self.detectives = []
-       self.x = "X"
+        self.boardmap = {}
+        self.x_history = []
+        self.detectives = []
+        self.x = None
+        self.load_board()
+        
+        
+        startTickets = {"taxi" : 10, "bus" : 8, "underground" : "4"}
+        
+        startLocs = []
+        with open("start_locations.txt", "r") as f:
+            for line in f:
+                startLocs.append(int(line.strip()))
+        random.shuffle(startLocs)    
+        
+        self.x = Player({"taxi" : 100, "bus": 100, "underground" : 100, "black" : 5, "2x" : 2}, startLocs[0], "X")
+
+        names = ["A", "B", "C", "D", "E"]
+
+        for n in range(len(names)):
+            self.detectives.append(Player(startTickets[:], startLocs[n + 1], names[n]))
+            
         
     def next_turn(self):
         pass # TODO: run the next turn
 
     def next_round(self):
         pass # TODO: run the next roundA
+
+    def load_board(self):
+        t = "t"
+        b = "b"
+        u = "u"
+        with open("board_data.txt", "r") as f:
+            for line in f:
+                data = [a.strip() for a in line.split("|")]
+                entry = {}
+                if len(data) > 1:
+                    entry[t] = [int(a.strip()) for a in data[1].split(" ")]
+                if len(data) > 2:
+                    entry[b] = [int(a.strip()) for a in data[2].split(" ")]
+                if len(data) > 3:
+                    entry[u] = [int(a.strip()) for a in data[3].split(" ")]
+                
+                blackTicket = []
+                for key in entry.keys():
+                    blackTicket += entry[key]
+    
+                entry["?"] = blackTicket
+                
+                if len(data) > 4:
+                    entry["?"] += [int(a.strip()) for a in data[4].split(' ')]
+     
+                if len(data) > 0:
+                    self.boardmap[int(data[0])] = entry
+
 
     # self.detectives: an array of Player objects, but only the detcvitvs
     # self.x: mr. x, a player object
--- engine/player.py
+++ engine/player.py
@@ -1,13 +1,14 @@
 #!/bin/python3
 
 class Player:
-    def __init__(self, tickets, pos):
+    def __init__(self, tickets, pos, name):
         self.tickets = tickets
         self.pos = pos
+        self.nextMove = None
+        self.name = name
 
     def __eq__(self, other):
-        return self.pos == other.pos and self.tickets == other.tickets
+        return self.pos == other.pos and self.tickets == other.tickets and self.name = other.name
 
     def move(self, transportation, new_nodeid):
-        # TODO: do some validation and then move, also fix tickets
-        pass
+        self.nextMove = (new_nodeid, transportation)
