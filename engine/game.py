3a131adaaf0d57b67e3fc032a149efe46ba7368a
1F335 <alexhayden25@gmail.com>
2018-06-03 15:23:27 -0400

    Begin adding game init stuff

--- engine/game.py
+++ engine/game.py
@@ -2,8 +2,9 @@
 
 class Game:
     def __init__(self, mr_x, detectives):
-        pass # TODO: do some initalization
-
+       self.detectives = []
+       self.x = "X"
+        
     def next_turn(self):
         pass # TODO: run the next turn
 
