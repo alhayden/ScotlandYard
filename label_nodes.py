3174648055e0649d824b8153144f11449a2fd14b
Dylan Jones <dylanjones2011@gmail.com>
2018-06-04 22:42:35 -0400

    label_nodes.py

---
+++ label_nodes.py
@@ -0,0 +1,20 @@
+from tkinter import Tk, Canvas
+from PIL import ImageTk, Image
+
+root = Tk()
+cnv = Canvas(root, width=1000, height=1000)
+img = Image.open("board.jpg")
+img = img.resize((1000, 1000))
+img = ImageTk.PhotoImage(image=img)
+cnv.create_image(500, 500, image=img)
+n = 1
+def callback(evt):
+    global n
+    print(n, evt.x, evt.y)
+    n += 1
+
+cnv.bind("<Button-1>", callback)
+
+cnv.pack()
+
+root.mainloop()
