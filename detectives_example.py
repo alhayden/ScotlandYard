c8827c2cac4808db1f1ee9de831e96f5aa193e32
Dylan Jones <dylanjones2011@gmail.com>
2018-06-07 09:37:26 -0400

    detectives don't kill each other now

--- detectives_example.py
+++ detectives_example.py
@@ -11,7 +11,12 @@ import random
 # Play Move, takes mr x and returns the move he wishes to make
 # Returns: the chosen move, a tuple ((int) new location, (string) transport type)
 def play_move(detective: Player, detectives: List[Player], x_history: List[Tuple[int, str]]) -> Tuple[int, str]:
-    # This fancy line just picks a random move based on the available tickets and location
-    transport = random.choice(list(filter(lambda x: x[1] > 0 and x[0] in boardmap[detective.pos].keys(), detective.tickets.items())))[0]
+    # This fancy line just shows which tickets we have that can be used
+    transport = map(lambda x: x[0], filter(lambda x: x[1] > 0 and x[0] in boardmap[detective.pos].keys(), detective.tickets.items()))
+    valid_nodes = []
+    for t in transport:
+        for neighbor in boardmap[detective.pos][t]:
+            if neighbor not in [d.pos for d in detectives]:
+                valid_nodes.append((neighbor, t))
     # And return a tuple: int, string
-    return random.choice(boardmap[detective.pos][transport]), transport
+    return random.choice(valid_nodes)
